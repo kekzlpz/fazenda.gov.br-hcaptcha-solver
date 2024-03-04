942f108cd8ea5828585b6f913684ca6bd4dbc950
kekzlpz <162126235+kekzlpz@users.noreply.github.com>
2024-03-04 17:32:50 +0100

    Update consultaRecaptcha.py

--- consultaRecaptcha.py
+++ consultaRecaptcha.py
@@ -9,7 +9,7 @@ capsolver.api_key = "Your pay per usage key"
 def solve_hcaptcha_nfe():
     solution = capsolver.solve({
         "type": "HCaptchaTask",
-        "websiteURL": "www.nfe.fazenda.gov.br",
+        "websiteURL": "https://www.nfe.fazenda.gov.br",
         "websiteKey": "e72d2f82-9594-4448-a875-47ded9a1898a",
         "proxy": PARSED_PROXY
     })
