f5208bcb50a57e72baac8418a47c6236673eccc8
kekzlpz <162126235+kekzlpz@users.noreply.github.com>
2024-03-04 17:28:53 +0100

    Create consultaRecaptcha.py

---
+++ consultaRecaptcha.py
@@ -0,0 +1,28 @@
+import capsolver
+from urllib.parse import urlparse
+
+# Change this information
+PARSED_PROXY = "http://username:password@host:port"
+
+capsolver.api_key = "Your pay per usage key"
+
+def solve_hcaptcha_nfe():
+    solution = capsolver.solve({
+        "type": "HCaptchaTask",
+        "websiteURL": "www.nfe.fazenda.gov.br",
+        "websiteKey": "e72d2f82-9594-4448-a875-47ded9a1898a",
+        "proxy": PARSED_PROXY
+    })
+    return solution
+
+def main():
+    
+    print("Solving nfe captcha")
+    solution = solve_hcaptcha_nfe()
+    
+    token = solution["gRecaptchaResponse"]
+    
+    print("Token Solution " + token)
+    
+if __name__ == "__main__":
+    main()
